a2806de59b035e46de208a5719e9bd7651aae365
Jason Mayer <flavorflavor@gmail.com>
2020-06-25 15:31:06 -0400

    updates

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -1,12 +1,51 @@
 const { gql } = require('apollo-server');
+const typeDefs = gql` 
 
-const typeDefs = gql`
- type Ports {
-        ID: Id!
-        NAME: Name!
-        CITY: City
-        STATE: State
+    type Trip {
+        id:ID!
+        utid: Utid
+        vessel: String!
+        efforts: Efforts
+        isboked: Boolean!
+    } 
+
+    type User {
+        id:ID!
+        utid: Utid
+        email: String!
+        tripid: [TripID]!
     }
+
+    type Utid {
+        uid: UID!
+        email: String!
+        tripid: [TripID]!
+    } 
+      
+    type Efforts {
+        name: String
+        effortsPatch(size: PatchSize): String
+    }
+  
+    enum PatchSize {
+        SMALL
+        LARGE
+    } 
+
+    type Query {
+        Trips: [Trip]!
+        trip(id: ID!): Trip
+        me: User
+      }  
+      
+      type Mutation {
+        bookTrips(launchIds: [ID]!): TripUpdateResponse!
+        cancelTrip(launchId: ID!): TripUpdateResponse!
+        login(email: String): String # login token
+      }
     
 `;
+
+
+
 module.exports = typeDefs;
